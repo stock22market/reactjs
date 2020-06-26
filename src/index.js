686d9d02e94c82fa135bf78e74f14d71841da40e
Paghdal <619238@cognizant.com>
2020-06-26 14:53:00 +0530

    test app 1

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import Store from './store';
 import './index.css';
-import { fetchTodos } from './actions/todo'
-
-const StoreInstance = Store();
-
-console.log('the store', StoreInstance)
-
-StoreInstance.dispatch(fetchTodos())
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={StoreInstance}>
-   <App />
- </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();

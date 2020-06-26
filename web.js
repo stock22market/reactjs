1cc8605e02c6ddfc483cce87317dfe7b6d47146e
Paghdal <619238@cognizant.com>
2020-06-26 12:30:07 +0530

    node js main file added

---
+++ web.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
